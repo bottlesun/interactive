0083d67a77711976b4c50ae8e91148e06486f1d1
bottlesun <bottlesun3514@gmail.com>
2022-05-30 15:41:19 +0900

    메인페이지 수정 22.05.30

--- 02.gsap_interative/js/main.js
+++ 02.gsap_interative/js/main.js
@@ -1,10 +1,12 @@
 let text = document.querySelector('h2');
 let char = text.querySelectorAll('span');
 
-let tl = gsap.timeline();
+let tl = gsap.timeline(),
+    ScrollAction = gsap.timeline() ,
+    navAciton = gsap.timeline()
 
 tl.set(char,{
-    yPercent:-110
+    yPercent:-150
 });
 
 tl.set(text,{
@@ -16,3 +18,32 @@ tl.to(char,{
     stagger : 0.05,
     ease : 'expo.inout',
 })
+
+navAciton.from('.navList ul li',{
+    duration: 1,
+    yPercent:-150,
+    stagger: 0.05,
+    ease: 'expo.inout'
+})
+
+ScrollAction.to('.main',1,{
+    scale : 2,
+    stagger:0.05,
+    ease:'expo.inout'
+})
+
+ScrollTrigger.create({
+    animation: ScrollAction,
+    trigger : ".main",
+    start: 'top 0%',
+    end : 'bottom 60%',
+    scrub : 2,
+    pin:true
+})
+ScrollTrigger.create({
+    animation : navAciton,
+    trigger : '.navList',
+    scrub : 2,
+    pin:true
+})
+
